a909f406df5bfafec3ad2375253a092ed09ea6ae
Casper <me@casperengelmann.com>
2019-07-25 13:17:18 +0200

    added eslint + fs-extra + update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
-import args from 'args';
+import fs from 'fs-extra';
+import prompts from 'prompts';
+import ProgressBar from 'progress';
 
-args
-  .option('port', 'The port on which the app will be running', 3000)
-  .option('reload', 'Enable/disable livereloading');
+(async () => {
+  const questions = [
+    {
+      type: 'text',
+      name: 'path',
+      message: 'Path to domain list',
+      validate: (path: string) => (fs.existsSync(path) ? true : 'Filen eksisterer ikke.'),
+    },
+    {
+      type: 'multiselect',
+      name: 'types',
+      message: 'Pick data types',
+      choices: [
+        { title: 'Name', value: true, selected: true },
+        { title: 'A', value: true, selected: true },
+        { title: 'NS', value: true },
+        { title: 'MX', value: true },
+      ],
+      max: 2,
+      hint: '- Space to select. Return to submit',
+    },
+  ];
+
+  const response = await prompts((questions as any));
+
+  console.log(response); // => { value: 24 }
+
+  // const bar = new ProgressBar('[:bar] :percent :eta seconds remaining', {
+  //   total: 100,
+  //   complete: '=',
+  //   incomplete: ' ',
+  //   width: 30,
+  // });
+  // const timer = setInterval(() => {
+  //   bar.tick();
+  //   if (bar.complete) {
+  //     clearInterval(timer);
+  //   }
+  // }, 100);
+})();
